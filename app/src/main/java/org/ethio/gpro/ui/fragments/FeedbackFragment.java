c5afdc5eec60fa8b864f886e80288ecf40924740
LeeCrey <SolomonBoloshe11x@gmail.com>
2022-11-29 09:29:44 +0300

    find by

--- app/src/main/java/org/ethio/gpro/ui/fragments/FeedbackFragment.java
+++ app/src/main/java/org/ethio/gpro/ui/fragments/FeedbackFragment.java
@@ -1,20 +1,53 @@
 package org.ethio.gpro.ui.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import org.ethio.gpro.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.ethio.gpro.databinding.FragmentFeedbackBinding;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class FeedbackFragment extends Fragment {
+    private FragmentFeedbackBinding binding;
+
+    private CircleImageView imageView;
+    private TextInputEditText feedback;
+    private TextView send;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_feedback, container, false);
+        binding = FragmentFeedbackBinding.inflate(inflater);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        feedback = binding.feedBack;
+        imageView = binding.currentUserProfilePicture;
+        send = binding.sendFeedback;
+        // TODO
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        binding = null;
+        feedback = null;
+        imageView = null;
+        send = null;
     }
 }
