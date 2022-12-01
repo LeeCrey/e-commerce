5af69696a3177ac7738fe577716914a6c2c8ae53
LeeCrey <SolomonBoloshe11x@gmail.com>
2022-12-01 12:03:41 +0300

    created

--- app/src/main/java/org/ethio/gpro/models/AccountResponse.java
+++ app/src/main/java/org/ethio/gpro/models/AccountResponse.java
@@ -1,4 +1,26 @@
 package org.ethio.gpro.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AccountResponse {
+    @JsonProperty("okay")
+    private Boolean okay;
+    @JsonProperty("message")
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getOkay() {
+        return okay;
+    }
+
+    public void setOkay(Boolean okay) {
+        this.okay = okay;
+    }
 }
