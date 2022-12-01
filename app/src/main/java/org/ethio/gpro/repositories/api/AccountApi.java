884b4e9eda8233d92ac98c0917c45124fe9c5b1e
LeeCrey <SolomonBoloshe11x@gmail.com>
2022-12-01 12:03:14 +0300

    created and added unlock and confirm methods

--- app/src/main/java/org/ethio/gpro/repositories/api/AccountApi.java
+++ app/src/main/java/org/ethio/gpro/repositories/api/AccountApi.java
@@ -1,4 +1,16 @@
 package org.ethio.gpro.repositories.api;
 
+import org.ethio.gpro.models.AccountResponse;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface AccountApi {
+
+    @POST("/customers/unlock")
+    Call<AccountResponse> unlock(@Query("unlock_token") String unlockToken);
+
+    @POST("/customers/confirmation")
+    Call<AccountResponse> confirm(@Query("confirmation_token") String confirmationToken);
 }
