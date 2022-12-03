8f9a66d94f9696bd7712e933167662942f96148e
LeeCrey <SolomonBoloshe11x@gmail.com>
2022-12-03 18:35:22 +0300

    added interceptors

--- app/src/main/java/org/ethio/gpro/repositories/retrofit/RetrofitConnectionUtil.java
+++ app/src/main/java/org/ethio/gpro/repositories/retrofit/RetrofitConnectionUtil.java
@@ -1,21 +1,36 @@
 package org.ethio.gpro.repositories.retrofit;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-import org.ethio.gpro.R;
+import org.ethio.gpro.helpers.ApplicationHelper;
+import org.ethio.gpro.helpers.CustomUrlHelper;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class RetrofitConnectionUtil {
+    public static final String HEADER_CACHE_CONTROL = "Cache-Control";
+    public static final String HEADER_PRAGMA = "Pragma";
+    private static final String TAG = "ServiceGenerator";
+    private static final long cacheSize = 5 * 1024 * 1024; // 5 MB
     private static Retrofit retrofit;
 
     public static synchronized Retrofit getRetrofitInstance(@NonNull Application application) {
@@ -23,18 +38,80 @@ public class RetrofitConnectionUtil {
             OkHttpClient client = new OkHttpClient.Builder()
                     .followRedirects(false)
                     .connectTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(20, TimeUnit.SECONDS)
-                    .callTimeout(2, TimeUnit.SECONDS)
-                    .readTimeout(20, TimeUnit.SECONDS)
+                    .cache(cache(application))
+                    .addInterceptor(httpLoggingInterceptor()) // used if network off OR on
+                    .addNetworkInterceptor(networkInterceptor()) // only used when network is on
+                    .addInterceptor(offlineInterceptor(application))
                     .build();
             ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.WRAP_ROOT_VALUE);
 
-            String finalUrl = application.getString(R.string.base_url);
+//            String finalUrl = application.getString(R.string.base_url);
+            String finalUrl = CustomUrlHelper.getUrl(application);
             retrofit = new Retrofit.Builder()
                     .baseUrl(finalUrl)
                     .addConverterFactory(JacksonConverterFactory.create(mapper))
                     .client(client).build();
         }
         return retrofit;
+    }
+
+    private static Interceptor offlineInterceptor(@NonNull Context context) {
+        return new Interceptor() {
+            @NonNull
+            @Override
+            public Response intercept(@NonNull Chain chain) throws IOException {
+                Request request = chain.request();
+
+                // prevent caching when network is on. For that we use the "networkInterceptor"
+                if (!ApplicationHelper.isInternetAvailable(context)) {
+                    CacheControl cacheControl = new CacheControl.Builder()
+                            .maxStale(7, TimeUnit.DAYS)
+                            .build();
+
+                    request = request.newBuilder()
+                            .removeHeader(HEADER_PRAGMA)
+                            .removeHeader(HEADER_CACHE_CONTROL)
+                            .cacheControl(cacheControl)
+                            .build();
+                }
+                return chain.proceed(request);
+            }
+        };
+    }
+
+    private static Interceptor networkInterceptor() {
+        return new Interceptor() {
+            @NonNull
+            @Override
+            public Response intercept(@NonNull Chain chain) throws IOException {
+                Response response = chain.proceed(chain.request());
+
+                CacheControl cacheControl = new CacheControl.Builder()
+                        .maxAge(5, TimeUnit.SECONDS)
+                        .build();
+
+                return response.newBuilder()
+                        .removeHeader(HEADER_PRAGMA)
+                        .removeHeader(HEADER_CACHE_CONTROL)
+                        .header(HEADER_CACHE_CONTROL, cacheControl.toString())
+                        .build();
+            }
+        };
+    }
+
+    private static HttpLoggingInterceptor httpLoggingInterceptor() {
+        HttpLoggingInterceptor httpLoggingInterceptor =
+                new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+                    @Override
+                    public void log(@NonNull String message) {
+                        Log.d(TAG, "log: http log: " + message);
+                    }
+                });
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return httpLoggingInterceptor;
+    }
+
+    private static Cache cache(Application application) {
+        return new Cache(new File(application.getCacheDir(), "someIdentifier"), cacheSize);
     }
 }
