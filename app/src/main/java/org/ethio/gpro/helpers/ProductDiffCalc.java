7b492f0ab0e8f8a504966b71aa0f6854830c7f38
LeeCrey <SolomonBoloshe11x@gmail.com>
2022-12-04 13:35:23 +0300

    added

--- app/src/main/java/org/ethio/gpro/helpers/ProductDiffCalc.java
+++ app/src/main/java/org/ethio/gpro/helpers/ProductDiffCalc.java
@@ -1,4 +1,43 @@
 package org.ethio.gpro.helpers;
 
-public class ProductDiffCalc {
+import androidx.recyclerview.widget.DiffUtil;
+
+import org.ethio.gpro.models.Product;
+
+import java.util.List;
+
+public class ProductDiffCalc extends DiffUtil.Callback {
+    private final List<Product> oldList;
+    private final List<Product> newList;
+
+    public ProductDiffCalc(List<Product> oldList, List<Product> newList) {
+        this.oldList = oldList;
+        this.newList = newList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldList == null ? 0 : oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newList == null ? 0 : newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        Product old = oldList.get(oldItemPosition);
+        Product newP = newList.get(newItemPosition);
+
+        return old.isSimilar(newP);
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        Product old = oldList.get(oldItemPosition);
+        Product newP = newList.get(newItemPosition);
+
+        return old.isContentTheSame(newP);
+    }
 }
